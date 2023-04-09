57633c5ec444f8dd0b9005d8e192471238dc68da
Manu <Manu@Daniel-PC>
2023-04-08 23:20:18 -0300

    Primeiro arquivo salvo no GitHub

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,6 +1,7 @@
 public class Exemplo {
 	public static void main(String [] args) {
-		System.out.println("Olá Mundo!");		
+		System.out.println("Olá Mundo!");
+		System.out.println("Enviar para GitHub");
 	}
 
 }
