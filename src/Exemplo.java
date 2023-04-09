b88053a5e73127b5e242c213b9a2966fb875bd32
Manu <Manu@Daniel-PC>
2023-04-09 00:11:02 -0300

    Primeiro arquivo da PUC enviado para GitHub

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -2,6 +2,7 @@ public class Exemplo {
 	public static void main(String [] args) {
 		System.out.println("Olá Mundo!");
 		System.out.println("Enviar para GitHub");
+		System.out.println("Agora vai dar certo pq gerei o token no GitHub");
 	}
 
 }
