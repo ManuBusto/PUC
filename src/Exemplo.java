680e1c0ae212137e79c6ca4603997032de222ac7
Manu <Manu@Daniel-PC>
2023-04-08 23:37:54 -0300

    fdfdsfsd

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -2,6 +2,7 @@ public class Exemplo {
 	public static void main(String [] args) {
 		System.out.println("Olá Mundo!");
 		System.out.println("Enviar para GitHub");
+		System.out.println("Vamos ver se agora vai");
 	}
 
 }
