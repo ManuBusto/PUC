b041a13ea110509f6f9c3eee5443a93e432e9f54
Manu <Manu@Daniel-PC>
2023-04-08 23:46:22 -0300

    Primeiro arquivo teste da PUC enviado para GitHub

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -2,7 +2,6 @@ public class Exemplo {
 	public static void main(String [] args) {
 		System.out.println("Olá Mundo!");
 		System.out.println("Enviar para GitHub");
-		System.out.println("Vamos ver se agora vai");
 	}
 
 }
